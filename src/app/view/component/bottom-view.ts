5f1e17baea2ebdb0100a6b25f33964d9646155b8
mark <mark@rgames.jp>
2021-04-07 14:49:56 +0900

    wip

--- src/app/view/component/bottom-view.ts
+++ src/app/view/component/bottom-view.ts
@@ -1,10 +1,10 @@
 import * as PIXI from 'pixi.js';
 
 import {View} from "../../../framework/view";
-import {Button} from "../../button";
 import event from "../../../framework/event";
 import {GameModel} from "../../model/game-model";
 import {EVENT_CLICK_PLAY} from "../../util/event";
+import buttonStyle from "../style/button-style";
 
 export class BottomView extends View {
   private _gameModel: GameModel;
@@ -16,27 +16,7 @@ export class BottomView extends View {
   }
 
   public init() {
-    const EE = new PIXI.utils.EventEmitter();
-
-    const style = new PIXI.TextStyle({
-      fontFamily: '04b03',
-      fontSize: 36,
-      fontStyle: 'italic',
-      fontWeight: 'bold',
-      fill: ['#ffffff', '#00ff99'], // gradient
-      stroke: '#4a1850',
-      strokeThickness: 5,
-      dropShadow: true,
-      dropShadowColor: '#000000',
-      dropShadowBlur: 4,
-      dropShadowAngle: Math.PI / 6,
-      dropShadowDistance: 6,
-      wordWrap: true,
-      wordWrapWidth: 440,
-    });
-
-
-    const playText = new Button('Spin');
+    const playText = new PIXI.Text('Spin', buttonStyle);
     playText.x = Math.round((this.vw - playText.width) / 2);
     playText.y = Math.round((this.vh - playText.height) / 2);
     playText.interactive = true;
--- src/app/view/component/top-view.ts
+++ src/app/view/component/top-view.ts
@@ -1,11 +1,12 @@
 import * as PIXI from 'pixi.js';
+import gsap from 'gsap';
 
 import {View} from "../../../framework/view";
 import {GameModel} from "../../model/game-model";
 import {SYMBOL_SIZE} from "../../util/env";
 import event from "../../../framework/event";
 import {EVENT_RENDER_AFTER_PLAY, EVENT_RENDER_PREPARE_PLAY} from "../../util/event";
-import gsap from 'gsap';
+import titleStyle from "../style/title-style";
 
 export class TopView extends View {
   private _gameModel: GameModel;
@@ -24,49 +25,32 @@ export class TopView extends View {
   }
 
   public init() {
-    const style = new PIXI.TextStyle({
-      fontFamily: '04b03',
-      fontSize: 36,
-      fontStyle: '',
-      fontWeight: 'bold',
-      fill: ['#ffffff', '#00ff99'], // gradient
-      stroke: '#4a1850',
-      strokeThickness: 5,
-      dropShadow: true,
-      dropShadowColor: '#000000',
-      dropShadowBlur: 4,
-      dropShadowAngle: Math.PI / 6,
-      dropShadowDistance: 6,
-      wordWrap: true,
-      wordWrapWidth: 440,
-    });
-
-    this._scoreText = new PIXI.Text('Score', style);
+    this._scoreText = new PIXI.Text('Score', titleStyle);
     this._scoreText.x = Math.round((this.vw - this._scoreText.width) / 2)  - SYMBOL_SIZE;
     this._scoreText.y = Math.round((this.vh) / 2);
     this.addChild(this._scoreText);
 
-    this._betText = new PIXI.Text('Bet', style);
+    this._betText = new PIXI.Text('Bet', titleStyle);
     this._betText.x = this._scoreText.x;
     this._betText.y = 0;
     this.addChild(this._betText);
 
-    this._scoreField = new PIXI.Text(`${this._gameModel.score}`, style);
+    this._scoreField = new PIXI.Text(`${this._gameModel.score}`, titleStyle);
     this._scoreField.x = Math.round((this.vw - this._scoreField.width) / 2)  + SYMBOL_SIZE * 0.9 - this._scoreField.width / 2;
     this._scoreField.y = Math.round((this.vh) / 2);
     this.addChild(this._scoreField);
 
-    this._scoreGainField = new PIXI.Text('', style);
+    this._scoreGainField = new PIXI.Text('', titleStyle);
     this._scoreGainField.x = Math.round((this.vw - this._scoreGainField.width) / 2)  + SYMBOL_SIZE * 1.4 - this._scoreGainField.width / 2;
     this._scoreGainField.y = Math.round((this.vh) / 2);
     this.addChild(this._scoreGainField);
 
-    this._betField = new PIXI.Text(`${this._gameModel.bet}`, style);
+    this._betField = new PIXI.Text(`${this._gameModel.bet}`, titleStyle);
     this._betField.x = Math.round((this.vw - this._betField.width) / 2)  + SYMBOL_SIZE * 0.9 - this._betField.width / 2;
     this._betField.y = 0
     this.addChild(this._betField);
 
-    this._betGainField = new PIXI.Text('', style);
+    this._betGainField = new PIXI.Text('', titleStyle);
     this._betGainField.x = Math.round((this.vw - this._betGainField.width) / 2)  + SYMBOL_SIZE * 1.4 - this._betGainField.width / 2;
     this._betGainField.y = 0
     this.addChild(this._betGainField);
